baf1ab9aae46a6cfe302c941d96d034e075c7f4a
Reki <rekirekouuu@gmail.com>
2025-03-03 22:51:11 +0100

    Jpgs are cropped, water tag is no longer shown.

--- src/main/java/com/transports/spring/model/templategeneration/common/TemplateJpg.java
+++ src/main/java/com/transports/spring/model/templategeneration/common/TemplateJpg.java
@@ -1,15 +1,18 @@
 package com.transports.spring.model.templategeneration.common;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.Path;
 
 import com.aspose.pdf.Document;
 import com.aspose.pdf.devices.*;
 import com.transports.spring.exception.GenerateJpgFromExcelException;
 
+import javax.imageio.ImageIO;
+
 public class TemplateJpg {
+
+    public static final int HEIGHT_TO_CROP = 150;
 
     protected TemplateJpg() {}
 
@@ -20,12 +23,28 @@ public class TemplateJpg {
             final OutputStream imageStream = createImageStream(involvedJpgCalendarPath);
             jpegDevice.process(document.getPages().get_Item(1), imageStream);
         }
+        cropImage(involvedJpgCalendarPath.toString());
     }
 
     private static OutputStream createImageStream(final Path involvedJpgCalendarPath) throws GenerateJpgFromExcelException {
         try {
             return new FileOutputStream(involvedJpgCalendarPath.toFile());
         } catch (final FileNotFoundException e) {
+            throw new GenerateJpgFromExcelException(e);
+        }
+    }
+
+    private static void cropImage(final String filePath) throws GenerateJpgFromExcelException {
+        final File imageFile = new File(filePath);
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(imageFile);
+            final int imageHeight = bufferedImage.getHeight() - HEIGHT_TO_CROP;
+            final int imageWidth = bufferedImage.getWidth();
+            final BufferedImage image = bufferedImage.getSubimage(0, HEIGHT_TO_CROP, imageWidth, imageHeight);
+            ImageIO.write(image, "jpg", imageFile);
+        }
+        catch (final IOException e) {
             throw new GenerateJpgFromExcelException(e);
         }
     }
