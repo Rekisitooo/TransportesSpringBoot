3dd78bd40a63299d45a4dc1328f6b4959675a312
Rekisitooo <quijano.iker@gmail.com>
2025-04-16 13:05:21 +0200

    Rows are dragged only when dragging from the first col

--- src/main/resources/static/js/own/templatecrud/animation/MoveTableRows.js
+++ src/main/resources/static/js/own/templatecrud/animation/MoveTableRows.js
@@ -1,27 +1,28 @@
 const rows = document.querySelectorAll('tbody tr');
 
 rows.forEach(row => {
-    row.addEventListener('dragstart', () => {
+    const dragHandle = row.querySelector('th[data-draggable=involvedTable]');
+
+    dragHandle.addEventListener('dragstart', () => {
         row.classList.add('dragging');
+        e.dataTransfer.setData('text/plain', null); // Required for Firefox
     });
 
-    row.addEventListener('dragend', () => {
+    dragHandle.addEventListener('dragend', () => {
         row.classList.remove('dragging');
     });
 
     row.addEventListener('dragover', (e) => {
-        e.preventDefault(); // Necesario para permitir el drop
+        e.preventDefault();
         const draggingRow = document.querySelector('.dragging');
-        const currentRow = row;
 
-        // Verifica si la fila actual es diferente a la que se está arrastrando
-        if (draggingRow !== currentRow) {
-            const bounding = currentRow.getBoundingClientRect();
+        if (draggingRow !== row) {
+            const bounding = row.getBoundingClientRect();
             const offset = bounding.y + bounding.height / 2;
             if (e.clientY - offset > 0) {
-                currentRow.parentNode.insertBefore(draggingRow, currentRow.nextSibling);
+                row.parentNode.insertBefore(draggingRow, row.nextSibling);
             } else {
-                currentRow.parentNode.insertBefore(draggingRow, currentRow);
+                row.parentNode.insertBefore(draggingRow, row);
             }
         }
     });
