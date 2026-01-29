f213ba3ef4bcfce17dab751ec8fd0b981e582ca7
Rekisitooo <quijano.iker@gmail.com>
2026-01-29 09:55:05 +0100

    Bug fixed. When no drivers available, show a text indicating it in the passenger cells

--- src/main/resources/static/js/own/transportcrud/driver/TransportCrudDriverAvailabilityForDateCRUD.js
+++ src/main/resources/static/js/own/transportcrud/driver/TransportCrudDriverAvailabilityForDateCRUD.js
@@ -1,29 +1,33 @@
 import { genericErrorAlert } from '../alert/GenericErrorAlert.js';
-import { changeElementDisplayNone, changeElementClass } from '../TransportCrudCommons.js';
+import { changeElementClass } from '../TransportCrudCommons.js';
 
-async function deleteDriverAssistance(data, passengerSelectsSelector, offersTransportIcon, driverPassengersDivId, notificationIconCol) {
-    const driverId = data.involvedId;
+async function deleteDriverAssistance(formData, iconsData) {
+    const driverId = formData.involvedId;
     // transport and notification deletions are done through a trigger
     await $.ajax({
         url: '/involvedAvailability',
         type: 'DELETE',
         contentType: 'application/json',
-        data: JSON.stringify(data),
+        data: JSON.stringify(formData),
         success: function(response) {
             //Quitar conductor de la tabla de viajeros
-            removeDriverInPassengerTableSelects(passengerSelectsSelector, driverId);
+            changePassengerTableSelects(iconsData.passengerSelectsSelector, driverId);
 
             //eliminar nombres de pasageros si los tiene
-            driverPassengersDivId.remove();
+            iconsData.driverPassengersDivId.remove();
 
             //cambia el icono de ofrece transporte
-            changeTransportOfferingIcon(0, offersTransportIcon, 'text-primary', 'text-muted');
+            changeTransportOfferingIcon(0, iconsData.offersTransportIcon, 'text-primary', 'text-muted');
 
             // quitar el icono de aviso
-            let notificationIconColClass = changeElementDisplayNone(notificationIconCol);
+            if (!iconsData.notificationIconCol.hasClass('d-none')) {
+                iconsData.notificationIconCol.addClass('d-none');
+            }
             // ponerlo en rojo si estaba en azul por si vuelve a estar disponible
-            notificationIconColClass = changeElementClass(notificationIconCol, 'text-primary', 'text-danger');
-            notificationIconCol.attr('class', notificationIconColClass);
+            if (!iconsData.notificationIconCol.hasClass('text-danger')) {
+                iconsData.notificationIconCol.addClass('text-primary');
+            }
+
             // marcar que el pasajero tiene el aviso pendiente
 
         },
@@ -33,22 +37,23 @@ async function deleteDriverAssistance(data, passengerSelectsSelector, offersTran
     });
 }
 
-async function createDriverAssistance(data, driverFullName, passengerSelectsSelector, offersTransportIcon, passengerNameCells, notificationIconCol) {
+async function createDriverAssistance(formData, iconsData) {
     await $.ajax({
         url: '/involvedAvailability',
         type: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify(data),
+        data: JSON.stringify(formData),
         success: function(response) {
             //poner el conductor en el combo de conductores los viajeros
-            addDriverInPassengerTable(passengerSelectsSelector, data, driverFullName, passengerNameCells);
+            addDriverInPassengerTable(formData, iconsData);
 
             //cambiar el iconito de ofrece transporte
-            changeTransportOfferingIcon(1, offersTransportIcon, 'text-muted', 'text-primary');
+            changeTransportOfferingIcon(1, iconsData.offersTransportIcon, 'text-muted', 'text-primary');
 
             // quitar el icono de aviso
-            let notificationIconColClass = changeElementDisplayNone(notificationIconCol);
-            notificationIconCol.attr('class', notificationIconColClass);
+            if (!iconsData.notificationIconCol.hasClass('d-none')) {
+                iconsData.notificationIconCol.addClass('d-none');
+            }
         },
         error: function(xhr, status, error) {
             genericErrorAlert();
@@ -56,52 +61,73 @@ async function createDriverAssistance(data, driverFullName, passengerSelectsSele
     });
 }
 
-function removeDriverInPassengerTableSelects(passengerSelectsSelector, driverId) {
-    $(passengerSelectsSelector).each(
+/**
+ * Remove driver from passenger selects in the passenger transports table
+ * and if there are no drivers left, hide the select and show the no drivers available span
+ * @param {*} passengerSelectsSelector 
+ * @param {*} driverId 
+ */
+function changePassengerTableSelects(passengerSelectsSelector, driverId) {
+    const $driverSelect = $(passengerSelectsSelector);
+
+    $driverSelect.each(
         function() {
-            const option = $(this).find('option[value="' + driverId + '"]');
-            if (option.is(':selected')) {
-                $(this).find('option:not([name=d])').attr('name', 'c');
+
+            $(this).find('option[value="' + driverId + '"]').remove();
+
+            // if there are no drivers left, hide the select and show the no drivers available span
+            if ($(this).find('option').length <= 1) {
+                // TODO if the passenger assists and needs transport, otherwise the select is hidden because the passenger doesn't need transport
+                $(this).addClass('d-none');
+                const noDriversAvailableSpanId = $(this).attr('id') + '_noDriversAvailableSpan';
+                noDriversAvailableSpanId.replace('_select', '');
+                $('#' + noDriversAvailableSpanId).removeClass('d-none');
             }
-            option.remove();
         }
     );
+
 }
 
-function addDriverInPassengerTable(passengerSelectsSelector, data, driverFullName, passengerNameCells) {
-    $(passengerSelectsSelector).each(
+function addDriverInPassengerTable(data, iconsData) {
+    const $driverSelect = $(iconsData.passengerSelectsSelector);
+
+    $driverSelect.each(
         function() {
-            const firstDriverOption = $(this).eq(1);
+            
+            // if there were no drivers available, show the select and hide the no drivers available span
+            if ($(this).hasClass('d-none')) {
+                // TODO if the passenger assists and needs transport, otherwise the select is hidden because the passenger doesn't need transport
+                $(this).removeClass('d-none');
+                const noDriversAvailableSpanId = $(this).attr('id') + '_noDriversAvailableSpan';
+                noDriversAvailableSpanId.replace('_select', '');
+                $('#' + noDriversAvailableSpanId).addClass('d-none');
+            }
 
             let operationToPerform;
-            if (firstDriverOption === null || firstDriverOption === undefined) {
+            const $selectedDriverOption = $(this).find('option:selected');
+            if ($selectedDriverOption === null || $selectedDriverOption === undefined) {
                 operationToPerform = 'c';
             } else {
-                operationToPerform = firstDriverOption.attr('name');
+                operationToPerform = 'd';
             }
 
             const passengerId = $(this).attr('data-t');
-            const passengerTh = passengerNameCells.filter('[data-t=' + passengerId + ']');
+            const passengerTh = iconsData.passengerNameCells.filter('[data-t=' + passengerId + ']');
             const passengerName = passengerTh.text();
-            const newDriverOption = $('<option>',
+            const $newDriverOption = $('<option>',
                 {
                     'value'                 :       data.involvedId,
                     'data-d'                :       data.transportDateId,
                     'data-t'                :       passengerId,
-                    'text'                  :       driverFullName,
+                    'text'                  :       iconsData.driverFullName,
                     'name'                  :       operationToPerform,
                     'data-passenger-name'   :       passengerName
                 }
             );
 
-            $(this).append(newDriverOption);
+            $(this).append($newDriverOption);
         }
     );
-}
-
-function changeElementDisplay(element) {
-    let elementClass = changeElementDisplayNone(element);
-    element.attr('class', elementClass);
 }
 
 function changeTransportOfferingIcon(dataDriverAssist, offersTransportIcon, classReplace, classToBeReplaced) {
@@ -115,27 +141,40 @@ async function changeDriverAssistance() {
     const offersTransportIcon = $(this);
     const driverId = offersTransportIcon.attr('data-d');
     const dateId = offersTransportIcon.attr('data-y');
-    const data = {
+    const formData = {
         transportDateId : dateId,
         involvedId : driverId,
     }
 
-    const passengerSelectsSelector = 'select[name=driverInTransportSelect][id*=' + data.transportDateId + '_select]';
+    const passengerSelectsSelector = 'select[name=driverInTransportSelect][id*=' + formData.transportDateId + '_select]';
     const notificationIconCol = $('#notificationIcon_' + driverId + '_' + dateId);
 
     const driverTransportOffer = offersTransportIcon.attr('data-driver-assist');
     if (driverTransportOffer === "1") {
         const driverPassengersDivId = $('div[id*=driverPassengersOnDate_' + driverId + '_' + dateId + ']');
-        await deleteDriverAssistance(data, passengerSelectsSelector, offersTransportIcon, driverPassengersDivId, notificationIconCol);
+        await deleteDriverAssistance(formData, 
+            {
+                passengerSelectsSelector : passengerSelectsSelector, 
+                offersTransportIcon : offersTransportIcon,
+                driverPassengersDivId : driverPassengersDivId, 
+                notificationIconCol : notificationIconCol
+            });
 
     } else if (driverTransportOffer === "0") {
         const driverFullName = $('#' + driverId + '_th').text();
         const passengerNameCells = $('th[id*=passengerNameCell_]');
-        await createDriverAssistance(data, driverFullName, passengerSelectsSelector, offersTransportIcon, passengerNameCells, notificationIconCol);
+        await createDriverAssistance(formData, 
+            {
+                driverFullName : driverFullName,
+                passengerSelectsSelector : passengerSelectsSelector,
+                offersTransportIcon : offersTransportIcon,
+                passengerNameCells : passengerNameCells,
+                notificationIconCol : notificationIconCol
+            });
     }
 }
 
-$(document).ready(function() {
+$(function() {
     $('#driverTransportsTable i[class*=car]').each(
         async function () {
             $(this).on('click', changeDriverAssistance);
